6b56d447a40face82da4d25517a5bd7d5cda234c
Low5tar <42311957+Low5tar@users.noreply.github.com>
2020-04-09 21:31:06 +0100

    Update quotes.rb

--- lib/plugins/quotes.rb
+++ lib/plugins/quotes.rb
@@ -2,11 +2,11 @@ module Quotes
   extend Discordrb::Commands::CommandContainer
 
   command(:trbmb) do |event|
-    event.respond JSON.parse(RestClient.get('http://api.chew.pro/trbmb'))[0]
+    event.respond JSON.parse(RestClient.get('http://api.kine.pro/trbmb'))[0]
   end
 
   command(:acronym, min_args: 1, max_args: 1) do |event, acro|
-    event.respond "#{acro} stands for: #{JSON.parse(RestClient.get(URI.escape("http://api.chew.pro/acronym/#{acro}")))['phrase']}"
+    event.respond "#{acro} stands for: #{JSON.parse(RestClient.get(URI.escape("http://api.kine.pro/acronym/#{acro}")))['phrase']}"
   end
 
   command(:quote, min_args: 1, max_args: 2) do |event, mesid, chanid = nil|
